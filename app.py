355c512fc4acf90be1f2aa3111d1b9e5ab5d5c09
Zhogno <116947415+Zhogno@users.noreply.github.com>
2024-09-25 13:06:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+     return "<h1 style='color:red'>Serhii Zhohno! 25.09.2024</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
